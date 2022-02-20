09b683347114de777dbe0c80cfbea0f2002fea90
leeyang_Mini <leeyang1991@gmail.com>
2022-02-20 19:09:30 +0800

    CCI_SM

--- preprocess.py
+++ preprocess.py
@@ -2,9 +2,9 @@
 import zipfile
 from __init__ import *
 # import ee
-
+global_land_tif = join(this_root,'conf/land.tif')
 global_start_year = 1982
-global_end_year = 2018
+# global_end_year = 2018
 # global_end_year = 2015
 global_season_dic = {
     'early':(3,4,5),
@@ -428,6 +428,243 @@ class GLC2000:
         plt.show()
         pass
 
+
+class CCI_SM:
+
+    def __init__(self):
+        self.datadir = join(data_root,'CCI')
+        pass
+
+    def run(self):
+        # self.nc_to_tif()
+        # self.daily_to_monthly()
+        # self.count_annual_tif_number()
+        # self.resample()
+        # self.per_pix()
+        # self.pick_early_peak_late()
+        self.interpolate_early_peak_late()
+        # self.check_cci_sm()
+        pass
+
+
+    def __nc_to_tif(self,nc):
+        ncin = Dataset(nc, 'r')
+        lat = ncin['lat']
+        lon = ncin['lon']
+        longitude_grid_distance = abs((lon[-1] - lon[0]) / (len(lon) - 1))
+        latitude_grid_distance = -abs((lat[-1] - lat[0]) / (len(lat) - 1))
+        longitude_start = lon[0]
+        latitude_start = lat[0]
+
+        start = datetime.datetime(1970, 1, 1)
+        time = ncin.variables['time']
+        date = start + datetime.timedelta(days=int(time[0]))
+        array = ncin['sm'][0][::]
+        array = np.array(array)
+        return date, array,longitude_start,latitude_start,longitude_grid_distance,latitude_grid_distance
+
+        pass
+
+    def nc_to_tif(self):
+        fdir = join(self.datadir,'ESACCI-SOILMOISTURE-L3S-SSMV-COMBINED_1978-2020-v06.1')
+        outdir = join(self.datadir,'tif_daily')
+        T.mk_dir(outdir)
+        for year_folder in T.listdir(fdir):
+            outdir_i = join(outdir,year_folder)
+            T.mk_dir(outdir_i)
+            for f in tqdm(T.listdir(join(fdir,year_folder)),desc=year_folder):
+                fpath = join(fdir,year_folder,f)
+                date, array,longitude_start,latitude_start,longitude_grid_distance,latitude_grid_distance = self.__nc_to_tif(fpath)
+                year = date.year
+                month = date.month
+                day = date.day
+                outf = f'{year}{month:02d}{day:02d}.tif'
+                outpath = join(outdir_i,outf)
+                newRasterfn = outpath
+                longitude_start = longitude_start
+                latitude_start = latitude_start
+                pixelWidth = longitude_grid_distance
+                pixelHeight = latitude_grid_distance
+                array = np.array(array)
+                array[array < 0] = np.nan
+                ToRaster().array2raster(newRasterfn, longitude_start, latitude_start, pixelWidth, pixelHeight, array)
+
+        pass
+
+    def daily_to_monthly(self):
+        fdir = join(self.datadir,'tif_daily')
+        outdir = join(self.datadir,'tif_monthly')
+        T.mk_dir(outdir)
+        for year_str in T.listdir(fdir):
+            month_dic = {}
+            for m in range(1,13):
+                key = f'{year_str}{m:02d}'
+                month_dic[key] = []
+            for f in T.listdir(join(fdir,year_str)):
+                yy_mm = f[:6]
+                month_dic[yy_mm].append(join(fdir,year_str,f))
+            for m in month_dic:
+                print(m)
+                flist = month_dic[m]
+                if len(flist) == 0:
+                    continue
+                outf = join(outdir,f'{m}.tif')
+                if isfile(outf):
+                    continue
+                Pre_Process().compose_tif_list(flist, outf)
+                # exit()
+
+    def count_annual_tif_number(self):
+        fdir = join(self.datadir, 'tif_daily')
+        for year_str in T.listdir(fdir):
+            flag = 0
+            for f in T.listdir(join(fdir, year_str)):
+                flag += 1
+            print(year_str,flag)
+        exit()
+        pass
+
+
+    def resample(self):
+        fdir = join(self.datadir,'tif_monthly')
+        outdir = join(self.datadir,'tif_monthly_05deg')
+        T.mk_dir(outdir)
+        for f in tqdm(T.listdir(fdir)):
+            outf = join(outdir,f)
+            fpath = join(fdir,f)
+            ToRaster().resample_reproj(fpath,outf,res=0.5)
+
+        pass
+
+
+    def per_pix(self):
+        fdir = join(self.datadir,'tif_monthly_05deg')
+        outdir = join(self.datadir,'per_pix/1982-2018')
+        T.mk_dir(outdir,force=True)
+        date_list = []
+        for y in range(1982,2019):
+            for m in range(1,13):
+                date_list.append(f'{y}{m:02d}.tif')
+        Pre_Process().data_transform_with_date_list(fdir,outdir,date_list)
+
+
+    def load_phenology_dic(self):
+        phenology_dir = join(data_root, 'Phenology')
+        early_start_dic = T.load_npy(join(phenology_dir, 'early_start_mon.npy'))
+        early_end_dic = T.load_npy(join(phenology_dir, 'early_end_mon.npy'))
+        late_end_dic = T.load_npy(join(phenology_dir, 'late_end_mon.npy'))
+        late_start_dic = T.load_npy(join(phenology_dir, 'late_start_mon.npy'))
+        dic_all = {
+            'early_start':early_start_dic,
+            'early_end':early_end_dic,
+            'late_start':late_start_dic,
+            'late_end':late_end_dic,
+        }
+
+        df = T.spatial_dics_to_df(dic_all)
+        early_range_list = []
+        peak_range_list = []
+        late_range_list = []
+        for i,row in tqdm(df.iterrows(),total=len(df)):
+            early_start = row.early_start
+            early_end = row.early_end
+            late_start = row.late_start
+            late_end = row.late_end
+            if len(early_start) == 0:
+                early_range_list.append(np.nan)
+                peak_range_list.append(np.nan)
+                late_range_list.append(np.nan)
+                continue
+            early_range = list(range(early_start[0],early_end[0]+1))
+            peak_range = list(range(early_end[0],late_start[0]+1))
+            late_range = list(range(late_start[0],late_end[0]+1))
+            early_range_list.append(early_range)
+            peak_range_list.append(peak_range)
+            late_range_list.append(late_range)
+        df['early_range'] = early_range_list
+        df['peak_range'] = peak_range_list
+        df['late_range'] = late_range_list
+        df = df.dropna()
+        phenology_dic = T.df_to_dic(df,'pix')
+        return phenology_dic
+
+
+    def pick_early_peak_late(self):
+        fdir = join(self.datadir,'per_pix/1982-2018')
+        outdir = join(self.datadir,'pick_early_peak_late')
+        T.mk_dir(outdir)
+        outf = join(outdir,'dataframe.df')
+        phenology_dic = self.load_phenology_dic()
+
+        dic = T.load_npy_dir(fdir)
+        result_dic = {}
+        for pix in tqdm(dic):
+            vals = dic[pix]
+            vals = np.array(vals)
+            vals = T.mask_999999_arr(vals,warning=False)
+            if np.isnan(np.nanmean(vals)):
+                continue
+            if not pix in phenology_dic:
+                continue
+            early_range = phenology_dic[pix]['early_range']
+            peak_range = phenology_dic[pix]['peak_range']
+            late_range = phenology_dic[pix]['late_range']
+            early_vals = T.monthly_vals_to_annual_val(vals,early_range)
+            peak_vals = T.monthly_vals_to_annual_val(vals,peak_range)
+            late_vals = T.monthly_vals_to_annual_val(vals,late_range)
+            dic_i = {
+                'early':early_vals,
+                'peak':peak_vals,
+                'late':late_vals,
+            }
+            result_dic[pix] = dic_i
+        df = T.dic_to_df(result_dic,'pix')
+        T.save_df(df,outf)
+        T.df_to_excel(df,outf)
+        pass
+
+    def interpolate_early_peak_late(self):
+        fdir = join(self.datadir,'pick_early_peak_late')
+        outdir = join(self.datadir,'pick_early_peak_late_npy')
+        T.mk_dir(outdir)
+        dff = join(fdir,'dataframe.df')
+        df = T.load_df(dff)
+        for season in global_season_dic:
+            spatial_dic = {}
+            outf = join(outdir,f'during_{season}_CCI_SM.npy')
+            for i,row in tqdm(df.iterrows(),total=len(df)):
+                pix = row['pix']
+                vals = row[season]
+                val_interp = T.interp_nan(vals,valid_percent=0.8)
+                if len(val_interp) == 1:
+                    continue
+                spatial_dic[pix] = val_interp
+            T.save_npy(spatial_dic,outf)
+
+    def check_cci_sm(self):
+        fdir = join(self.datadir, 'pick_early_peak_late')
+        dff = join(fdir, 'dataframe.df')
+        df = T.load_df(dff)
+        # spatial_dic = T.df_to_spatial_dic(df,'early')
+        spatial_dic = T.df_to_spatial_dic(df,'peak')
+        spatial_dic_num = {}
+        for pix in spatial_dic:
+            vals = spatial_dic[pix]
+            if np.isnan(np.nanmean(vals)):
+                continue
+            val_valid = T.remove_np_nan(vals)
+            valid_num = len(val_valid)
+            if not valid_num == 444//12:
+                continue
+            spatial_dic_num[pix] = 1
+
+        arr = DIC_and_TIF().pix_dic_to_spatial_arr(spatial_dic_num)
+        DIC_and_TIF().arr_to_tif(arr,join(this_root,'conf/sm_valid.tif'))
+        # DIC_and_TIF().plot_back_ground_arr(global_land_tif)
+        # plt.imshow(arr)
+        # plt.colorbar()
+        # plt.show()
+
 def seasonal_split_ly_NDVI():
     fdir = join(data_root, 'NDVI_ly/per_pix')
     outdir = join(data_root, 'NDVI_ly/per_pix_seasonal')
@@ -450,14 +687,13 @@ def seasonal_split_ly_NDVI():
 
 
 
-
-
 def main():
     # LAI().run()
     # seasonal_split_ly_NDVI()
     # GEE_AVHRR_LAI().run()
-    GLC2000().run()
-    # read_abl()
+    # GLC2000().run()
+    CCI_SM().run()
+    # check_cci_sm()
     # f = '/Volumes/NVME2T/greening_project_redo/data/GEE_AVHRR_LAI/per_pix_clean/per_pix_dic_005.npy'
     # dic = T.load_npy(f)
     # for pix in dic:
