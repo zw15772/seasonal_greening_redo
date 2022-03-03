f2af30ea484e51e4407c6eb40aec56362ca1f4f3
leeyang_Mini <leeyang1991@gmail.com>
2022-03-03 22:46:57 +0800

    Phenology

--- Main_flow_1.py
+++ Main_flow_1.py
@@ -36,6 +36,370 @@ class Global_vars:
         relative_change_list = np.array(relative_change_list)
         return relative_change_list
 
+class Phenology:
+
+    def __init__(self):
+        self.this_class_arr, self.this_class_tif, self.this_class_png = T.mk_class_dir('Phenology',
+                                                                                       results_root_main_flow)
+
+
+    def run(self):
+        # 1 把多年的NDVI分成单年，分南北半球，分文件夹存储
+        # fdir = data_root+'NDVI_phenology/tif_05deg_bi_weekly/'
+        # outdir = data_root+'NDVI_phenology/tif_05deg_bi_weekly_separate/'
+        # self.split_north_south_hemi(fdir,outdir)
+
+        # 2 把单年的NDVI tif 转换成 perpix
+        # for folder in ['north','south_modified']:
+        #     fdir = data_root+'NDVI_phenology/tif_05deg_bi_weekly_separate/{}/'.format(folder)
+        #     outdir = data_root+'NDVI_phenology/per_pix_separate/{}/'.format(folder)
+        #     self.data_transform_split_files(fdir,outdir)
+        # 3 hants smooth
+        # self.hants()
+        # self.check_hants()
+
+        # 4 计算 top left right
+        # self.SOS_EOS()
+        # self.check_SOS_EOS()
+
+        # 5 合成南北半球
+        # self.compose_SOS_EOS()
+
+        # 6 check sos
+        # self.check_sos()
+        pass
+
+
+
+    def compose_SOS_EOS(self):
+        outdir = self.this_class_arr + 'compose_SOS_EOS/'
+        T.mk_dir(outdir)
+        threshold_i = 0.5
+        SOS_EOS_dic = DIC_and_TIF().void_spatial_dic()
+        for hemi in ['north','south_modified']:
+            fdir = self.this_class_arr + 'SOS_EOS/threshold_{}/{}/'.format(threshold_i, hemi)
+            for f in T.listdir(fdir):
+                # year = f.split('.')[0]
+                # year = int(year)
+                # print fdir + f
+                dic = T.load_npy(fdir + f)
+                for pix in dic:
+                    vals = dic[pix]
+                    vals = np.array(vals)
+                    a,b = pix.split('.')
+                    a = int(a)
+                    b = int(b)
+                    pix = (a,b)
+                    SOS_EOS_dic[pix].append(vals)
+        SOS_EOS_dic_np = {}
+        for pix in SOS_EOS_dic:
+            val = SOS_EOS_dic[pix]
+            val = np.array(val)
+            SOS_EOS_dic_np[pix] = val
+        np.save(outdir + 'compose_SOS_EOS',SOS_EOS_dic_np)
+        # spatial_dic = {}
+        # for pix in SOS_EOS_dic:
+        #     vals = SOS_EOS_dic[pix]
+        #     length = len(vals)
+        #     spatial_dic[pix] = length
+        # arr = DIC_and_TIF().pix_dic_to_spatial_arr(spatial_dic)
+        # plt.imshow(arr)
+        # plt.show()
+            # exit()
+        pass
+
+    def split_north_south_hemi(self,fdir,outdir):
+        # 1 north
+        north_dir = outdir + 'north/'
+        south_dir = outdir + 'south/'
+        T.mk_dir(south_dir,force=True)
+        T.mk_dir(north_dir,force=True)
+        years = np.array(range(1982, 2016))
+        for y in tqdm(years):
+            for f in T.listdir(fdir):
+                if not f.endswith('tif'):
+                    continue
+                year = int(f[:4])
+                if y == year:
+                    date = f[4:8]
+                    # print date
+                    # exit()
+                    array,originX,originY,pixelWidth,pixelHeight = to_raster.raster2array(fdir+f)
+                    array_south = copy.copy(array)
+                    array_north = copy.copy(array)
+                    array_south[:180] = -999999.
+                    array_north[180:] = -999999.
+                    north_outdir_y = north_dir + '{}/'.format(y)
+                    south_outdir_y = south_dir + '{}/'.format(y)
+                    Tools().mk_dir(north_outdir_y)
+                    Tools().mk_dir(south_outdir_y)
+                    south_outf = south_outdir_y + date + '.tif'
+                    north_outf = north_outdir_y + date + '.tif'
+                    DIC_and_TIF().arr_to_tif(array_south,south_outf)
+                    DIC_and_TIF().arr_to_tif(array_north,north_outf)
+        # 2 modify south
+        south_modified_dir = outdir + 'south_modified/'
+        T.mk_dir(south_modified_dir)
+        for year in T.listdir(south_dir):
+            outdir_y = south_modified_dir + '{}/'.format(year)
+            T.mk_dir(outdir_y)
+            for f in T.listdir(south_dir + year):
+                mon = f.split('.')[0][:2]
+                day = f.split('.')[0][2:]
+                year = int(year)
+                mon = int(mon)
+                # print 'original date:',year,mon
+                old_fname = south_dir + str(year) + '/' + f
+                mon = mon - 6
+                if mon <= 0:
+                    year_new = year - 1
+                    mon_new = mon + 12
+                    new_fname = south_modified_dir + '{}/{:02d}{}.tif'.format(year_new,mon_new,day)
+                else:
+                    new_fname = south_modified_dir + '{}/{:02d}{}.tif'.format(year,mon,day)
+
+                # print old_fname
+                # print new_fname
+                try:
+                    shutil.copy(old_fname,new_fname)
+                except Exception as e:
+                    # print e
+                    continue
+
+
+
+    def split_files(self,fdir,outdir):
+        Tools().mk_dir(outdir)
+        years = np.array(range(1982, 2016))
+        for y in tqdm(years):
+            for f in T.listdir(fdir):
+                year = int(f[:4])
+                if y == year:
+                    outdir_y = outdir + '{}/'.format(y)
+                    Tools().mk_dir(outdir_y)
+                    shutil.copy(fdir + f, outdir_y + f)
+
+
+    def data_transform(self, fdir, outdir):
+        # 不可并行，内存不足
+        Tools().mk_dir(outdir)
+        # 将空间图转换为数组
+        # per_pix_data
+        flist = T.listdir(fdir)
+        date_list = []
+        for f in flist:
+            if f.endswith('.tif'):
+                date = f.split('.')[0]
+                date_list.append(date)
+        date_list.sort()
+        all_array = []
+        for d in tqdm(date_list, 'loading...'):
+            # for d in date_list:
+            for f in flist:
+                if f.endswith('.tif'):
+                    if f.split('.')[0] == d:
+                        # print(d)
+                        array, originX, originY, pixelWidth, pixelHeight = to_raster.raster2array(fdir + f)
+                        array = np.array(array, dtype=float)
+                        # print np.min(array)
+                        # print type(array)
+                        # plt.imshow(array)
+                        # plt.show()
+                        all_array.append(array)
+
+        row = len(all_array[0])
+        col = len(all_array[0][0])
+
+        void_dic = {}
+        void_dic_list = []
+        for r in range(row):
+            for c in range(col):
+                void_dic['%03d.%03d' % (r, c)] = []
+                void_dic_list.append('%03d.%03d' % (r, c))
+
+        # print(len(void_dic))
+        # exit()
+        params = []
+        for r in tqdm(range(row)):
+            for c in range(col):
+                for arr in all_array:
+                    val = arr[r][c]
+                    void_dic['%03d.%03d' % (r, c)].append(val) # TODO: need to be transformed into tuple
+
+        # for i in void_dic_list:
+        #     print(i)
+        # exit()
+        flag = 0
+        temp_dic = {}
+        for key in tqdm(void_dic_list, 'saving...'):
+            flag += 1
+            # print('saving ',flag,'/',len(void_dic)/100000)
+            temp_dic[key] = void_dic[key]
+            if flag % 10000 == 0:
+                # print('\nsaving %02d' % (flag / 10000)+'\n')
+                np.save(outdir + 'per_pix_dic_%03d' % (flag / 10000), temp_dic)
+                temp_dic = {}
+        np.save(outdir + 'per_pix_dic_%03d' % 0, temp_dic)
+
+    def data_transform_split_files(self,fdir,outdir):
+        for year in T.listdir(fdir):
+            # print year
+            fdir_i = fdir + year + '/'
+            outdir_i = outdir + year + '/'
+            Tools().mk_dir(outdir_i, force=1)
+            self.data_transform(fdir_i, outdir_i)
+
+        pass
+
+    def kernel_hants(self, params):
+        outdir, y, fdir = params
+        outdir_y = outdir + y + '/'
+        Tools().mk_dir(outdir_y, force=1)
+        for f in T.listdir(fdir + y):
+            dic = dict(np.load(fdir + y + '/' + f).item())
+            hants_dic = {}
+            for pix in dic:
+                vals = dic[pix]
+                vals = np.array(vals)
+                std = np.std(vals)
+                if std == 0:
+                    continue
+                xnew, ynew = self.__interp__(vals)
+                ynew = np.array([ynew])
+                # print np.std(ynew)
+                results = HANTS(sample_count=365, inputs=ynew, low=-10000, high=10000,
+                                fit_error_tolerance=std)
+                result = results[0]
+
+                # plt.plot(result)
+                # plt.plot(range(len(ynew[0])),ynew[0])
+                # plt.show()
+                hants_dic[pix] = result
+            np.save(outdir_y + f, hants_dic)
+
+    def hants(self):
+        for hemi in ['north','south_modified']:
+            outdir = self.this_class_arr + 'hants_smooth/{}/'.format(hemi)
+            fdir = data_root + 'NDVI_phenology/per_pix_separate/{}/'.format(hemi)
+            params = []
+            for y in T.listdir(fdir):
+                params.append([outdir, y, fdir])
+                # self.kernel_hants([outdir, y, fdir])
+            MULTIPROCESS(self.kernel_hants, params).run(process=5)
+
+    def check_hants(self):
+        hemi = 'south_modified'
+        fdir = self.this_class_arr + 'hants_smooth/{}/'.format(hemi)
+        tropical_mask_dic = NDVI().tropical_mask_dic
+
+        for year in T.listdir(fdir):
+            perpix_dir = fdir + '{}/'.format(year)
+            for f in T.listdir(perpix_dir):
+                if not '021' in f:
+                    continue
+                dic = T.load_npy(perpix_dir + f)
+                for pix in dic:
+                    if pix in tropical_mask_dic:
+                        continue
+                    vals = dic[pix]
+                    if len(vals) > 0:
+                        # print pix,vals
+                        plt.plot(vals)
+                        plt.show()
+                        sleep()
+            exit()
+
+    def __interp__(self, vals):
+
+        # x_new = np.arange(min(inx), max(inx), ((max(inx) - min(inx)) / float(len(inx))) / float(zoom))
+
+        inx = range(len(vals))
+        iny = vals
+        x_new = np.linspace(min(inx), max(inx), 365)
+        func = interpolate.interp1d(inx, iny)
+        y_new = func(x_new)
+
+        return x_new, y_new
+
+    def __search_left(self, vals, maxind, threshold_i):
+        left_vals = vals[:maxind]
+        left_min = np.min(left_vals)
+        max_v = vals[maxind]
+        threshold = (max_v - left_min) * threshold_i + left_min
+
+        ind = 999999
+        for step in range(365):
+            ind = maxind - step
+            if ind >= 365:
+                break
+            val_s = vals[ind]
+            if val_s <= threshold:
+                break
+
+        return ind
+
+    def __search_right(self, vals, maxind, threshold_i):
+        right_vals = vals[maxind:]
+        right_min = np.min(right_vals)
+        max_v = vals[maxind]
+        threshold = (max_v - right_min) * threshold_i + right_min
+
+        ind = 999999
+        for step in range(365):
+            ind = maxind + step
+            if ind >= 365:
+                break
+            val_s = vals[ind]
+            if val_s <= threshold:
+                break
+
+        return ind
+
+    def SOS_EOS(self, threshold_i=0.5):
+        for hemi in ['north','south_modified']:
+            out_dir = self.this_class_arr + 'SOS_EOS/threshold_{}/{}/'.format(threshold_i,hemi)
+            Tools().mk_dir(out_dir, force=1)
+            # fdir = data_root + 'NDVI_phenology/HANTS/'
+            fdir = self.this_class_arr + 'hants_smooth/{}/'.format(hemi)
+            for y in tqdm(T.listdir(fdir)):
+                year_dir = fdir + y + '/'
+                result_dic = {}
+                for f in T.listdir(year_dir):
+                    dic = dict(np.load(year_dir + f).item())
+                    for pix in dic:
+                        try:
+                            vals = dic[pix]
+                            maxind = np.argmax(vals)
+                            start = self.__search_left(vals, maxind, threshold_i)
+                            end = self.__search_right(vals, maxind, threshold_i)
+                            result = [start,maxind, end]
+                            result_dic[pix] = result
+                            # print result
+                            # sleep()
+                        except:
+                            pass
+                            # plt.plot(vals)
+                            # plt.show()
+                            # exit()
+                        # plt.plot(vals)
+                        # plt.plot(range(start,end),vals[start:end],linewidth=4,zorder=99,color='r')
+                        # plt.title('start:{} \nend:{} \nduration:{}'.format(start,end,end-start))
+                        # plt.show()
+                np.save(out_dir + y, result_dic)
+
+    def check_SOS_EOS(self,threshold_i=0.5):
+        fdir = self.this_class_arr + 'SOS_EOS/threshold_{}/north/'.format(threshold_i)
+        for f in T.listdir(fdir):
+            dic = T.load_npy(fdir+f)
+            spatial_dic = {}
+            for pix in dic:
+                SOS = dic[pix][0]
+                pix = (int(pix.split('.')[0]),int(pix.split('.')[1]))
+                spatial_dic[pix] = SOS
+            arr = DIC_and_TIF().pix_dic_to_spatial_arr(spatial_dic)
+            plt.imshow(arr)
+            plt.show()
+        pass
 
 class Get_Monthly_Early_Peak_Late:
 
@@ -517,11 +881,111 @@ class RF:
         return df
 
 
+class Analysis:
+
+    def __init__(self):
+        self.this_class_arr, self.this_class_tif, self.this_class_png = T.mk_class_dir('Analysis',results_root_main_flow)
+
+        pass
+
+    def run(self):
+        # self.Greeing_trend()
+        self.Greeing_trend_two_period()
+        pass
+
+
+    def nan_linear_fit(self, val1_list, val2_list):
+        # pearson correlation of val1 and val2, which contain Nan
+        K = KDE_plot()
+        val1_list_new = []
+        val2_list_new = []
+        for i in range(len(val1_list)):
+            val1 = val1_list[i]
+            val2 = val2_list[i]
+            if np.isnan(val1):
+                continue
+            if np.isnan(val2):
+                continue
+            val1_list_new.append(val1)
+            val2_list_new.append(val2)
+        if len(val1_list_new) <= 3:
+            a, b, r, p = np.nan, np.nan, np.nan, np.nan
+        else:
+            a, b, r, p = K.linefit(val1_list_new, val2_list_new)
+        return a, b, r, p
+    def Greeing_trend(self):
+        var_ = 'LAI_3g'
+        outdir = join(self.this_class_tif,f'Greeing_trend/{var_}')
+        T.mk_dir(outdir,force=True)
+        dff = join(Pick_Early_Peak_Late_value().this_class_arr,f'Pick_variables/{var_}.df')
+        df = T.load_df(dff)
+        for season in global_season_dic:
+            print(season)
+            col_name = join(f'{season}')
+            spatial_dic = T.df_to_spatial_dic(df,col_name)
+            trend_dic = {}
+            for pix in spatial_dic:
+                vals = spatial_dic[pix]
+                x = list(range(len(vals)))
+                y = vals
+                a, b, r, p = self.nan_linear_fit(x,y)
+                trend_dic[pix] = a
+            out_tif = join(outdir,f'{season}.tif')
+            DIC_and_TIF().pix_dic_to_tif(trend_dic,out_tif)
+            # arr = DIC_and_TIF().pix_dic_to_spatial_arr(trend_dic)
+            # plt.figure()
+            # plt.imshow(arr,vmax=0.02,vmin=-0.02,cmap='RdBu')
+            # plt.colorbar()
+            # DIC_and_TIF().plot_back_ground_arr(Global_vars().land_tif)
+            # plt.title(season)
+        # plt.show()
+        pass
+
+    def Greeing_trend_two_period(self):
+        var_ = 'LAI_3g'
+        period_1 = list(range(1982,2002))
+        period_2 = list(range(2002,2019))
+        year_list = list(range(1982,2021))
+        period_list = [period_1,period_2]
+        outdir = join(self.this_class_tif,f'Greeing_trend_two_period/{var_}')
+        T.mk_dir(outdir,force=True)
+        dff = join(Pick_Early_Peak_Late_value().this_class_arr,f'Pick_variables/{var_}.df')
+        df = T.load_df(dff)
+        flag = 0
+        for period in period_list:
+            flag += 1
+            for season in global_season_dic:
+                print(season,flag)
+                col_name = join(f'{season}')
+                spatial_dic = T.df_to_spatial_dic(df,col_name)
+                trend_dic = {}
+                for pix in spatial_dic:
+                    vals = spatial_dic[pix]
+                    period = np.array(period)
+                    period_index = period - global_start_year
+                    vals_period = T.pick_vals_from_1darray(vals,period_index)
+                    x = list(range(len(vals_period)))
+                    y = vals_period
+                    a, b, r, p = self.nan_linear_fit(x,y)
+                    trend_dic[pix] = a
+                out_tif = join(outdir,f'{flag}_{season}.tif')
+                DIC_and_TIF().pix_dic_to_tif(trend_dic,out_tif)
+                # arr = DIC_and_TIF().pix_dic_to_spatial_arr(trend_dic)
+                # plt.figure()
+                # plt.imshow(arr,vmax=0.02,vmin=-0.02,cmap='RdBu')
+                # plt.colorbar()
+                # DIC_and_TIF().plot_back_ground_arr(Global_vars().land_tif)
+                # plt.title(season)
+            # plt.show()
+        pass
+
 
 def main():
+    Phenology().run()
     # Get_Monthly_Early_Peak_Late().run()
     # Pick_Early_Peak_Late_value().run()
-    RF().run()
+    # RF().run()
+    # Analysis().run()
 
 
 if __name__ == '__main__':
