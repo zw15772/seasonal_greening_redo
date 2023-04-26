06c29504b30bf6ff764f9ff53e847d337ad3dce3
Yang-M2 <leeyang1991@gmail.com>
2023-04-26 18:49:38 +0800

    Amplifying_Stablilizing plot ratio

--- ly_scripts/amplifying_stablilizing.py
+++ ly_scripts/amplifying_stablilizing.py
@@ -1,10 +1,77 @@
 # coding=utf-8
 import matplotlib.pyplot as plt
 from lytools import *
+import matplotlib as mpl
+
+plt.rcParams['font.sans-serif'] = ['Arial']
+plt.rcParams['font.size'] = 8
+centimeter_factor = 1 / 2.54
 
 T = Tools()
 
-this_script_root = '/Users/liyang/Downloads/earlier_late/'
+this_script_root = '/Users/liyang/Desktop/earlier_late/'
+data_root = this_script_root + 'data/'
+
+class Dataframe_Func:
+
+    def __init__(self,df):
+        self.df = self.add_Humid_nonhumid(df)
+
+    def run(self):
+
+        pass
+
+
+    def P_PET_ratio(self):
+        fdir = join(data_root,'aridity_P_PET_dic')
+        # fdir = P_PET_fdir
+        dic = T.load_npy_dir(fdir)
+        dic_long_term = {}
+        for pix in dic:
+            vals = dic[pix]
+            vals = np.array(vals)
+            vals = T.mask_999999_arr(vals,warning=False)
+            vals[vals == 0] = np.nan
+            if len(vals) == 0:
+                continue
+            if np.isnan(np.nanmean(vals)):
+                continue
+            vals = T.drop_n_std(vals)
+            long_term_vals = np.nanmean(vals)
+            dic_long_term[pix] = long_term_vals
+        return dic_long_term
+
+    def P_PET_class(self,):
+        dic = self.P_PET_ratio()
+        dic_reclass = {}
+        for pix in dic:
+            val = dic[pix]
+            label = None
+            # label = np.nan
+            if val > 0.65:
+                label = 'Humid'
+                # label = 3
+            elif val < 0.2:
+                label = 'Arid'
+                # label = 0
+            elif val > 0.2 and val < 0.5:
+                label = 'Semi Arid'
+                # label = 1
+            elif val > 0.5 and val < 0.65:
+                label = 'Semi Humid'
+                # label = 2
+            dic_reclass[pix] = label
+        return dic_reclass
+
+    def add_Humid_nonhumid(self,df):
+        P_PET_dic_reclass = self.P_PET_class()
+        df = T.add_spatial_dic_to_df(df,P_PET_dic_reclass,'HI_reclass')
+        df = T.add_spatial_dic_to_df(df, P_PET_dic_reclass, 'HI_class')
+        df = df.dropna(subset=['HI_class'])
+        df.loc[df['HI_reclass'] != 'Humid', ['HI_reclass']] = 'Dryland'
+        return df
+
+
 
 class Amplifying_Stablilizing:
     def __init__(self):
@@ -20,7 +87,11 @@ class Amplifying_Stablilizing:
 
         # self.class_tif()
         # self.plot_tif()
-        self.ratio_statistic()
+        # self.ratio_statistic()
+        # self.individual_model_ratio()
+        self.plot_ratio()
+
+        pass
 
     def class_tif(self):
         outdir = join(self.this_class_tif,'class_tif')
@@ -58,7 +129,8 @@ class Amplifying_Stablilizing:
         T.mk_dir(outdir)
         color_list = ['purple', '#FFFFCC', 'g']
         cmap = T.cmap_blend(color_list)
-        plt.register_cmap(name='mycmap', cmap=cmap)
+        # plt.register_cmap(name='mycmap', cmap=cmap)
+        mpl.colormaps.register(name='mycmap',cmap=cmap)
         for f in T.listdir(fdir):
             if not 'MODIS' in f:
                 continue
@@ -74,6 +146,9 @@ class Amplifying_Stablilizing:
         T.open_path_and_file(outdir)
 
     def ratio_statistic(self):
+        # water and energy limited
+        outdir = join(self.this_class_arr,'ratio_statistic')
+        T.mkdir(outdir)
         fdir = join(self.this_class_tif,'class_tif')
         all_dict = {}
         for f in T.listdir(fdir):
@@ -83,15 +158,163 @@ class Amplifying_Stablilizing:
             all_dict[key] = dict_i
         df = T.spatial_dics_to_df(all_dict)
         df = df.dropna()
-        spatial_dict = {}
-        for i,row in df.iterrows():
-            pix = row['pix']
-            spatial_dict[pix] = 1
-        arr = DIC_and_TIF().pix_dic_to_spatial_arr(spatial_dict)
-        plt.imshow(arr,interpolation='nearest',cmap='gray')
-        plt.show()
+        df = Dataframe_Func(df).df
+        HI_list = ['Humid','Dryland']
+        amp_stab_dict = {
+            -1:'strong stablilizing',
+            0:'weak stablilizing',
+            1:'amplifying'
+        }
+        T.print_head_n(df)
+        # cols = df.columns.tolist()
+        cols = ['LAI3g','MODIS_LAI','Trendy_ensemble']
+        all_results_dict = {}
+        for HI in HI_list:
+            df_HI = df[df['HI_reclass'] == HI]
+            for col in cols:
+                spatial_dict = T.df_to_spatial_dic(df_HI,col)
+                count_dict = {
+                    'strong stablilizing':0,
+                    'weak stablilizing':0,
+                    'amplifying':0,
+                }
+                total = 0
+                for pix in spatial_dict:
+                    val_i = spatial_dict[pix]
+                    class_i = amp_stab_dict[val_i]
+                    count_dict[class_i] += 1
+                    total += 1
+                for class_i in count_dict:
+                    count_dict[class_i] = count_dict[class_i]/total * 100
+                key_i = f'{HI}_{col}'
+                all_results_dict[key_i] = count_dict
+        df = pd.DataFrame(all_results_dict)
+        outf = join(outdir, 'ratio_statistic.df')
+        T.save_df(df, outf)
+        T.df_to_excel(df, outf)
+        T.open_path_and_file(outdir)
+        # T.print_head_n(df)
+        # exit()
 
         pass
+
+    def individual_model_ratio(self):
+        outdir = join(self.this_class_arr, 'individual_models')
+        T.mkdir(outdir)
+        fdir = join(self.this_class_tif, 'individual_models')
+        all_dict = {}
+        key_list = []
+        for f in tqdm(T.listdir(fdir)):
+            fpath = join(fdir, f)
+            dict_i = DIC_and_TIF().spatial_tif_to_dic(fpath)
+            key = f.replace('.tif', '')
+            key_list.append(key)
+            all_dict[key] = dict_i
+        # print(key_list)
+        # exit()
+        df = T.spatial_dics_to_df(all_dict)
+        # df = df.dropna()
+        df = Dataframe_Func(df).df
+        HI_list = ['Humid', 'Dryland']
+        amp_stab_dict = {
+            -1: 'strong stablilizing',
+            0: 'weak stablilizing',
+            1: 'amplifying'
+        }
+        T.print_head_n(df)
+        # exit()
+        # cols = df.columns.tolist()
+        cols = key_list
+        all_results_dict = {}
+        for HI in HI_list:
+            df_HI = df[df['HI_reclass'] == HI]
+            for col in cols:
+                spatial_dict = T.df_to_spatial_dic(df_HI, col)
+                count_dict = {
+                    'strong stablilizing': 0,
+                    'weak stablilizing': 0,
+                    'amplifying': 0,
+                }
+                total = 0
+                for pix in spatial_dict:
+                    val_i = spatial_dict[pix]
+                    if np.isnan(val_i):
+                        continue
+                    class_i = amp_stab_dict[val_i]
+                    count_dict[class_i] += 1
+                    total += 1
+                for class_i in count_dict:
+                    count_dict[class_i] = count_dict[class_i] / total * 100
+                key_i = f'{HI}_{col}'
+                all_results_dict[key_i] = count_dict
+        df = pd.DataFrame(all_results_dict)
+        outf = join(outdir, 'ratio_statistic.df')
+        T.save_df(df, outf)
+        T.df_to_excel(df, outf)
+        T.open_path_and_file(outdir)
+        pass
+
+
+    def plot_ratio(self):
+        outdir = join(self.this_class_png,'ratio_statistic')
+        T.mkdir(outdir)
+        fpath1 = join(self.this_class_arr,'ratio_statistic','ratio_statistic.df')
+        fpath2 = join(self.this_class_arr, 'individual_models/ratio_statistic.df')
+        df1 = T.load_df(fpath1)
+        df2 = T.load_df(fpath2)
+        df = pd.concat([df1,df2],axis=1)
+        # T.print_head_n(df)
+        # exit()
+        # T.print_head_n(df)
+        HI_list = ['Humid', 'Dryland']
+        amp_stab_dict = {
+            -1: 'strong stablilizing',
+            0: 'weak stablilizing',
+            1: 'amplifying'
+        }
+        # T.print_head_n(df)
+        # cols = df.columns.tolist()
+        cols1 = ['LAI3g', 'MODIS_LAI', 'Trendy_ensemble']
+        cols2 = ['CABLE-POP_S2_lai', 'CLASSIC_S2_lai', 'CLM5',
+                'IBIS_S2_lai', 'ISAM_S2_LAI', 'ISBA-CTRIP_S2_lai',
+                'LPJ-GUESS_S2_lai', 'LPX-Bern_S2_lai', 'OCN_S2_lai',
+                'ORCHIDEE_S2_lai', 'ORCHIDEEv3_S2_lai', 'VISIT_S2_lai',
+                'YIBs_S2_Monthly_lai']
+        color_dict = {
+            'LAI3g':'#1f77b4',
+            'MODIS_LAI':'#ff7f0e',
+            'Trendy_ensemble':'#2ca02c',
+        }
+        cols1.extend(cols2)
+        cols = cols1
+        smp_stab_list = ['strong stablilizing','weak stablilizing','amplifying']
+
+        for HI in HI_list:
+            plt.figure(figsize=(18*centimeter_factor, 5*centimeter_factor))
+            flag = 1
+            for smp_stab in smp_stab_list:
+                plt.subplot(1,3,flag)
+                flag += 1
+                x = []
+                y = []
+                color_list = []
+                for col in cols:
+                    key_i = f'{HI}_{col}'
+                    color = color_dict[col] if col in color_dict else 'grey'
+                    val_i = df.loc[smp_stab,key_i]
+                    x.append(col)
+                    y.append(val_i)
+                    color_list.append(color)
+                plt.bar(x,y,color=color_list)
+                plt.title(f'{HI} {smp_stab}')
+                plt.xticks([])
+                plt.ylim(0,85)
+            outf = join(outdir,f'{HI}.pdf')
+            plt.savefig(outf)
+            plt.close()
+        T.open_path_and_file(outdir)
+
+
 
     def __get_model_list(self):
         models_list = []
